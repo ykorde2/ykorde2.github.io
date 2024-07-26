d63841df9ef810f068370cf09bdf9d952c36d491
ykorde2 <ykorde2@illinois.edu>
2024-07-26 17:08:35 -0400

    state where it works?

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,455 @@
+// First Slide
+async function renderFirstChart() {
+    const margin = {top: 10, right: 20, bottom: 30, left: 50},
+        width = 1000 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+    const data = await d3.csv("https://rohitmukherjee.github.io/data/1-annual-working-hours-vs-gdp-per-capita-pwt.csv");
+    const year = 2015
+    const filteredData = data.filter(function (d) {
+        return d.year == year && d.total_population != "" && d.average_annual_hours_worked != "" && d.gdp_per_capita != "";
+    });
+
+    let svg = d3.select("#chart-1").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add X axis
+    const x = d3.scaleLinear()
+        .domain([1000, 70000])
+        .range([0, width]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickFormat(d => d + " $/yr"));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([1200, 2800])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y).tickFormat(d => d + " hr"));
+
+    // Add a scale for bubble size
+    const z = getBubbleSizeScale()
+
+    // Add a scale for bubble color
+    const myColor = d3.scaleOrdinal()
+        .domain(getContinentKeys())
+        .range(d3.schemeSet2);
+
+    // -1- Create a tooltip div that is hidden by default:
+    const tooltip = d3.select("#slide-1")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("padding", "10")
+        .style("color", "white")
+        .style("width", "150px")
+        .style("height", "50px")
+
+    // Add dots
+    svg.append('g')
+        .selectAll("scatterplot-dot")
+        .data(filteredData)
+        .enter()
+        .append("circle")
+        .attr("class", "bubbles")
+        .attr("cx", function (d) {
+            return x(Number(d.gdp_per_capita));
+        })
+        .attr("cy", function (d) {
+            return y(Number(d.average_annual_hours_worked));
+        })
+        .attr("id", function (d) {
+            return "bubble-" + d.code;
+        })
+        .attr("r", function (d) {
+            return z(Number(d.total_population));
+        })
+        .on("mouseover", function (event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html(firstChartTooltipHTML(d));
+            tooltip.style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px")
+        })
+        .on("mouseout", function (d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        })
+        .style("fill", function (d) {
+            return myColor(d.continent);
+        });
+    renderLegend(svg, getContinentKeys(), width, myColor);
+    countryCodesToAnnotate().forEach(function (countryCode) {
+        for (let i = 0; i < filteredData.length; i++) {
+            if (filteredData[i].code === countryCode) {
+                const countryData = filteredData[i];
+                renderFirstChartAnnotations(countryData, x(Number(countryData.gdp_per_capita)), y(Number(countryData.average_annual_hours_worked)), margin);
+            }
+        }
+    })
+}
+
+function renderFirstChartAnnotations(d, x, y, margin) {
+    const computedDX = d.entity == "France" ? -30 : 30;
+    const computedDY = d.entity == "France" ? 30 : -30;
+    const annotations = [
+        {
+            note: {
+                label: "$" + Math.round(d.gdp_per_capita) + "/year, " + Math.round(d.average_annual_hours_worked) + " hrs/yr",
+                lineType: "none",
+                bgPadding: {"top": 15, "left": 10, "right": 10, "bottom": 10},
+                title: d.entity,
+                orientation: "leftRight",
+                "align": "middle"
+            },
+            type: d3.annotationCallout,
+            subject: {radius: 30},
+            x: x,
+            y: y,
+            dx: computedDX,
+            dy: computedDY
+        },
+    ];
+    const makeAnnotations = d3.annotation().annotations(annotations);
+
+    d3.select("svg")
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "annotation-group")
+        .call(makeAnnotations)
+}
+
+function renderSecondChartAnnotations(d, x, y, margin) {
+    const computedDX = d.entity == "France" ? -30 : 30;
+    const computedDY = d.entity == "France" ? 30 : -30;
+    const annotations = [
+        {
+            note: {
+                label: Math.round(d.productivity) + " $/hour, " + Math.round(d.average_annual_hours_worked) + " hrs/yr",
+                lineType: "none",
+                bgPadding: {"top": 15, "left": 10, "right": 10, "bottom": 10},
+                title: d.entity,
+                orientation: "leftRight",
+                "align": "middle"
+            },
+            type: d3.annotationCallout,
+            subject: {radius: 30},
+            x: x,
+            y: y,
+            dx: computedDX,
+            dy: computedDY
+        },
+    ];
+    const makeAnnotations = d3.annotation().annotations(annotations);
+
+    d3.select("svg")
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "annotation-group")
+        .call(makeAnnotations)
+}
+
+function renderThirdChartAnnotations(d, x, y, margin) {
+    d3.select(".annotation-group").remove();
+    const annotations = [
+        {
+            note: {
+                label: "An average worker makes " + Math.round(d.productivity) + " $/hour",
+                lineType: "none",
+                bgPadding: {"top": 15, "left": 10, "right": 10, "bottom": 10},
+                title: d.entity,
+                orientation: "topBottom",
+                align: "top"
+            },
+            type: d3.annotationCalloutCircle,
+            subject: {radius: 30},
+            x: x,
+            y: y,
+            dx: -100,
+            dy: -10
+        },
+    ];
+    const makeAnnotations = d3.annotation().annotations(annotations);
+    const chart = d3.select("svg")
+    chart.transition()
+        .duration(1000);
+    chart.append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "annotation-group")
+        .call(makeAnnotations)
+}
+
+function firstChartTooltipHTML(object) {
+    return "<div>" + object.entity + "</div><div>$" + Math.round(object.gdp_per_capita) + "/year</div><div>" + Math.round(object.average_annual_hours_worked) + " hrs worked yearly</div>";
+}
+
 function countryCodesToAnnotate() {
     return ["CHN", "IND", "USA", "RUS", "KOR"]
+}
+
+// Second Slide
+async function renderSecondChart() {
+    const margin = {top: 10, right: 20, bottom: 30, left: 50},
+        width = 1000 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+    const data = await d3.csv("https://rohitmukherjee.github.io/data/3-productivity-vs-annual-hours-worked.csv");
+    const year = 2015
+    const filteredData = data.filter(function (d) {
+        return d.year == year && d.total_population != "" && d.average_annual_hours_worked != "" && d.productivity != "";
+    });
+
+    console.log(filteredData);
+
+    let svg = d3.select("#chart-2").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add X axis
+    const x = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, width]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickFormat(d => d + " $/hr"));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([1200, 2800])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y).tickFormat(d => d + " hr"));
+    const z = getBubbleSizeScale();
+
+    // Add a scale for bubble color
+    const myColor = d3.scaleOrdinal()
+        .domain(getContinentKeys())
+        .range(d3.schemeSet2);
+
+    // -1- Create a tooltip div that is hidden by default:
+    const tooltip = d3.select("#chart-2")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("color", "white")
+        .style("width", "150px")
+        .style("height", "50px")
+
+    // Add dots
+    svg.append('g')
+        .selectAll("dot")
+        .data(filteredData)
+        .enter()
+        .append("circle")
+        .attr("class", "bubbles")
+        .attr("id", function (d) {
+            return "bubble-" + d.code;
+        })
+        .attr("cx", function (d) {
+            return x(Number(d.productivity));
+        })
+        .attr("cy", function (d) {
+            return y(Number(d.average_annual_hours_worked));
+        })
+        .attr("r", function (d) {
+            return z(Number(d.total_population));
+        })
+        .style("fill", function (d) {
+            return myColor(d.continent);
+        })
+        // -3- Trigger the functions
+        .on("mouseover", function (event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html(secondChartTooltipHTML(d));
+            tooltip.style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px")
+        })
+        .on("mouseout", function (d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        })
+        .style("fill", function (d) {
+            return myColor(d.continent);
+        });
+    renderLegend(svg, getContinentKeys(), width, myColor);
+    countryCodesToAnnotate().forEach(function (countryCode) {
+        for (let i = 0; i < filteredData.length; i++) {
+            if (filteredData[i].code === countryCode) {
+                const countryData = filteredData[i];
+                renderSecondChartAnnotations(countryData, x(Number(countryData.productivity)), y(Number(countryData.average_annual_hours_worked)), margin);
+            }
+        }
+    })
+}
+
+function secondChartTooltipHTML(object) {
+    return "<div>" + object.entity + "</div><div>$" + Math.round(object.productivity) + "\/hour</div><div>" + Math.round(object.average_annual_hours_worked) + " hrs worked yearly</div>";
+}
+
+// Fourth slide
+function renderFourthChart() {
+    // The svg
+    const svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+// Map and projection
+    const projection = d3.geoMercator()
+        .scale(70)
+        .center([0, 20])
+        .translate([width / 2, height / 2]);
+
+    const tooltip = d3.select("#slide-4")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("padding", "10")
+        .style("color", "white")
+        .style("width", "150px")
+        .style("height", "50px")
+
+// Data and color scale
+    let data = new Map()
+    const colorScale = d3.scaleOrdinal()
+        .domain(getContinentKeys())
+        .range(d3.schemeSet2);
+
+// Load external data and boot
+    Promise.all([
+        d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+        d3.csv("https://rohitmukherjee.github.io/data/1-annual-working-hours-vs-gdp-per-capita-pwt.csv", function (d) {
+            if (d.year == 2015) {
+                data.set(d.code,
+                    {
+                        year: d.year,
+                        gdp_per_capita: Number(d.gdp_per_capita),
+                        name: d.entity,
+                        population: d.total_population,
+                        continent: d.continent
+                    });
+            }
+        })
+    ]).then(function (loadData) {
+        let topo = loadData[0]
+
+        // Draw the map
+        svg.append("g")
+            .selectAll("path")
+            .data(topo.features)
+            .join("path")
+            // draw each country
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+            // set the color of each country
+            .attr("fill", function (d) {
+                if (!data.has(d.id)) {
+                    return 0;
+                } else {
+                    return colorScale(data.get(d.id).continent);
+                }
+            })
+            .on("mouseover", function (event, d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html(fourthChartTooltipHTML(data.get(d.id)));
+                tooltip.style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px")
+            })
+            .on("mouseout", function (d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            })
+    })
+
+    renderLegend(d3.select("#chart-4"), getContinentKeys(), width, colorScale);
+
+}
+
+function fourthChartTooltipHTML(object) {
+    return "<div>" + object.name + "</div><div>" + object.population + " people</div><div>$" + Math.round(object.gdp_per_capita) + "/year</div>";
+}
+
+// Common functions
+function getBubbleSizeScale() {
+    // Add a scale for bubble size
+    const z = d3.scaleLog()
+        .domain([200000, 1310000000])
+        .range([1, 30]);
+    return z;
+}
+
+function renderLegend(svg, continentKeys, width, myColor) {
+    // Add one dot in the legend for each name.
+    svg.selectAll("legend-dots")
+        .data(continentKeys)
+        .enter()
+        .append("circle")
+        .attr("cx", width - 100)
+        .attr("cy", function (d, i) {
+            return 50 + i * 25
+        }) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("r", 2)
+        .style("fill", function (d) {
+            return myColor(d)
+        })
+
+    svg.selectAll("legend-labels")
+        .data(continentKeys)
+        .enter()
+        .append("text")
+        .attr("x", width + 8 - 100)
+        .attr("y", function (d, i) {
+            return 50 + i * 25
+        }) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function (d) {
+            return myColor(d)
+        })
+        .text(function (d) {
+            return d
+        })
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
 }
 
 function getContinentKeys() {
     return ["Asia", "Europe", "North America", "South America", "Africa", "Oceania"];
 }
 
+function getEntities() {
+    return ["Argentina", "Australia", "Austria", "Bangladesh", "Barbados", "Belgium", "Brazil", "Bulgaria", "Cambodia", "Canada", "Chile", "China",
+        "Colombia", "Costa Rica", "Croatia", "Cyprus", "Czechia", "Denmark", "Ecuador", "Estonia", "Finland", "France", "Germany", "Greece", "Hong Kong",
+        "Hungary", "Iceland", "India", "Indonesia", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Latvia", "Lithuania", "Luxembourg", "Malaysia", "Malta",
+        "Mexico", "Myanmar", "Netherlands", "New Zealand", "Nigeria", "Norway", "Pakistan", "Peru", "Philippines", "Poland", "Portugal", "Romania", "Russia",
+        "Saint Lucia", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka",
+        "Sweden", "Switzerland", "Taiwan", "Thailand", "Trinidad and Tobago", "Turkey", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam"]
+}
 
 async function renderFifthChart() {
     const margin = {top: 10, right: 20, bottom: 30, left: 50},
-        width = 1200 - margin.left - margin.right,
+        width = 1000 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
     const data = await d3.csv("https://ykorde2.github.io/data/change-heat-deaths-gdp.csv");  // Update with the URL or path to your new CSV file
     const year = 2015;  // Considering GDP per capita in 2022
@@ -56,7 +496,7 @@ async function renderFifthChart() {
 
     // Add X axis
     const x = d3.scaleLog()
-        .domain([700, 120000])  // Adjust domain as necessary
+        .domain([750, 120000])  // Adjust domain as necessary
         .range([0, width])
         .base(10);
         const xTicks = [1000, 2000, 5000, 10000, 20000, 50000, 100000];
@@ -178,7 +618,7 @@ function renderFifthChartAnnotations(d, x, y, margin) {
 
 async function renderHeatDeathRateChart() {
     const margin = {top: 10, right: 20, bottom: 30, left: 50},
-        width = 1200 - margin.left - margin.right,
+        width = 1000 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
 
     const data = await d3.csv("https://ykorde2.github.io/data/change-heat-death-rate.csv");
@@ -293,7 +733,7 @@ function renderHeatDeathRateAnnotations(d, x, y, margin) {
 
 async function renderHeatDeathRateChart2() {
     const margin = {top: 10, right: 20, bottom: 30, left: 50},
-        width = 1200 - margin.left - margin.right,
+        width = 1000 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
 
 
@@ -432,7 +872,7 @@ async function renderHeatDeathRateChart2() {
 
 async function renderSixthChart() {
     const margin = {top: 10, right: 20, bottom: 30, left: 50},
-        width = 1200 - margin.left - margin.right,
+        width = 1000 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
 
     // Load CSV data
